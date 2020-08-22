37168e9e6924dfa788e097ac9d917e2161ef0b48
Bluette1 <marylene.sawyer@gmail.com>
2020-08-22 15:53:44 +0200

    Fix lint errors

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,32 +1,32 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe ApplicationHelper, :type => :helper do
-  let(:user){User.create(name: "name")}
-  
+RSpec.describe ApplicationHelper, type: :helper do
+  let(:user) { User.create(name: 'name') }
+
   before :each do
     assign(:current_user, user)
   end
 
-  describe "#logged_in_user" do
-    it "returns the correct string" do
+  describe '#logged_in_user' do
+    it 'returns the correct string' do
       expect(helper.logged_in_user).to eq('Logged_in user: ')
     end
   end
 
-  describe "#current_user_name" do
-    it "returns the current user name" do
+  describe '#current_user_name' do
+    it 'returns the current user name' do
       expect(helper.current_user_name).to eq(user.name)
     end
   end
 
-  describe "#current_user_id" do
-    it "returns the current user id" do
+  describe '#current_user_id' do
+    it 'returns the current user id' do
       expect(helper.current_user_id).to eq(user.id)
     end
   end
 
-  describe "#current_user" do
-    it "returns the current user" do
+  describe '#current_user' do
+    it 'returns the current user' do
       expect(helper.current_user).to eq(user)
     end
   end
--- spec/helpers/session_helper_spec.rb
+++ spec/helpers/session_helper_spec.rb
@@ -1,14 +1,14 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe SessionsHelper, :type => :helper do
-  let(:user){User.create(name: "name")}
-  
+RSpec.describe SessionsHelper, type: :helper do
+  let(:user) { User.create(name: 'name') }
+
   before :each do
     assign(:current_user, user)
   end
 
-  describe "#welcome" do
-    it "returns the correct string" do
+  describe '#welcome' do
+    it 'returns the correct string' do
       expect(helper.welcome).to eq("Nice to see you again, #{user.name}!")
     end
   end
