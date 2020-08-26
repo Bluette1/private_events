a207c6dcfea556a767a05ce022a71f1e45cad848
Bluette1 <marylene.sawyer@gmail.com>
2020-08-26 13:31:50 +0200

    Correct `describe` block, add variable for `name`

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe 'User Requests', type: :request do
-  describe 'when user is not logged in ' do
+  let(:name) { { name: 'new_name' } }
+  describe 'when user is not logged in' do
     it 'retrieves the signup page' do
       get '/sign_up'
       expect(response).to be_successful
@@ -19,15 +20,15 @@ RSpec.describe 'User Requests', type: :request do
     end
 
     it 'creates a new user successfully and redirects to the show page' do
-      post '/users', params: { user: { name: 'new_name' } }
+      post '/users', params: { user: name }
       expect(response).to redirect_to(user_path(assigns(:user).id))
       expect(response.code).to eq '302'
       expect(response).to have_http_status(:found)
       expect(User.find_by(name: 'new_name')).to_not eq nil
     end
-
+  end
+  describe 'when user is logged in' do
     it 'renders the correct show page' do
-      name = { name: 'new_name' }
       post '/users', params: { user: name }
       post '/sign_in', params: name
       user_id = User.find_by(name).id
